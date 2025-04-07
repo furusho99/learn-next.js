2757ee56c614a6227b636efa45757683b25c1bac
furusho99 <shota1445@icloud.com>
2025-04-07 18:31:59 +0900

    Chapter03 done

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+//Lusitana
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
