6ddfef1a2ea8a045fae312a9a1a610a1eeb20b4d
furusho99 <xguye2514@gmail.com>
2025-04-15 17:13:53 +0900

    Chapter13 done

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -25,10 +25,12 @@ export async function createInvoice(formData: FormData) {
 	const amountInCents = amount * 100;
 	const [date] = new Date().toISOString().split("T");
 
-	await sql`
+	try {
+		await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+	} catch (error) {}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
@@ -45,11 +47,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 	const amountInCents = amount * 100;
 
-	await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+	try {
+		await sql`
+		UPDATE invoices
+		SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+		WHERE id = ${id}
+	  `;
+	} catch (error) {}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
